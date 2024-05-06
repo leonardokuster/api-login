73e46a7959a95e8077f6e3398f6ac989798a74a9
Leonardo Cristiano Kuster <l.kusterr@gmail.com>
2024-05-06 14:04:38 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -2,9 +2,6 @@ const express = require("express");
 const cors = require('cors');
 const app = express();
 const bodyParser = require("body-parser");
-const NodeCache = require("node-cache");
-
-const cache = new NodeCache();
 
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
@@ -24,23 +21,8 @@ app.use(bodyParser.json());
 
 
 const cadastros = require("../controllers/cadastros");
+app.use('/escritoriokuster', cadastros);
 
-app.use('/escritoriokuster', async (req, res, next) => {
-  const cacheKey = req.originalUrl;
-  const cachedData = cache.get(cacheKey);
-  
-  if (cachedData) {
-      return res.json(cachedData);
-  } else {
-      try {
-          const data = await cadastros(req, res);
-          cache.set(cacheKey, data, 60000); 
-          return res.json(data);
-      } catch (error) {
-          return res.status(500).json({ error: true, message: "Erro interno do servidor" });
-      }
-  }
-});
 
 const PORT = process.env.PORT || 5656;
 app.listen(PORT, () => {
