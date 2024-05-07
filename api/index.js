150b78fd46f88f09205800e2c7bfc9f43909aada
Leonardo Cristiano Kuster <l.kusterr@gmail.com>
2024-05-07 10:27:09 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -13,7 +13,7 @@ app.use((req, res, next) => {
 
 app.use(cors());
 
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, '../views'));
 app.set('view engine', 'ejs');
 
 app.use(express.static('public'));
