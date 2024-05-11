42881a7deebc802b89f298c0a243f38c490e14d8
Leonardo Cristiano Kuster <l.kusterr@gmail.com>
2024-05-10 23:00:55 -0300

    Update index.js

--- api/routes/index.js
+++ api/routes/index.js
@@ -4,6 +4,8 @@ const cors = require('cors');
 const usuario = require('./usuarioRoute');
 
 module.exports = app => {
+  const express = require('express');
+  const app = express();
 
   app.use(bodyParser.urlencoded({extended: false}));
   app.use(bodyParser.json());
