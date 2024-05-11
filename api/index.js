0b9537e5dcacfdfb284ded1bf4f1bfc28c388d5e
Leonardo Cristiano Kuster <l.kusterr@gmail.com>
2024-05-11 10:58:36 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+app.use(expressLayouts);
 app.set('view engine', 'ejs');
 app.engine('ejs', require('ejs').__express);
 app.set('views', path.join(__dirname, 'views'));
-
 
 routes(app);
 
